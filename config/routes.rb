3c1af6b3bbd0d2fbc2b2d257c2ccfd652852741d
Talon.Zhou <ztyemail@126.com>
2017-01-08 15:44:00 +0800

    创建了项目的主页，即商品列表页

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+	root "store#index"
 end
