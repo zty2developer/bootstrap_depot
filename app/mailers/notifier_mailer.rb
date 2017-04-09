d5990c1cab3f74fbf1e9e1c80c4364da79d10fae
Talon.Zhou <ztyemail@126.com>
2017-04-09 13:16:46 +0800

    删除了文本类型的邮件模板

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -9,7 +9,7 @@ class NotifierMailer < ApplicationMailer
   def order_received(order)
 		@order = order
 
-		mail to: order.email, subject: "Pragmatic Store Order Confirmation"
+		mail to: @order.email, subject: "Pragmatic Store Order Confirmation"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
