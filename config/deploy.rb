b25ba5bfd6bfd4a5c12c53b7c0b102c5cec728d6
Talon.Zhou <ztyemail@126.com>
2017-04-10 16:26:06 +0800

    Set branch parameter for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
